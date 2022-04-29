5e801832e5cb6f5e6ada6c565cc564c329fc6659
audreylory <47520459+audreylory@users.noreply.github.com>
2022-04-29 17:53:47 +0200

    quelques modifs sur connection

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  username = "Audrey";
+  password = "1234";
+  message = false;
+  isAuthenticated = false;
+
+  //Service à créer
+  constructor(private router: Router, private productService: ProductsService) { }
 
   ngOnInit(): void {
+  }
+
+  login(logForm: any){
+    if(logForm.value.username == this.username && logForm.value.password == this.password){
+      this.router.navigate(["courses"]);
+      this.productService.isAuthenticated = true;
+    }else{
+      this.message = true;
+    }
   }
 
 }
