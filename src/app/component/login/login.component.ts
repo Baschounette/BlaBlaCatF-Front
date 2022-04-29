cb4766a4964aea3bc607e68c2ed15c54f1e5af80
Baschounette <78910786+Baschounette@users.noreply.github.com>
2022-04-29 18:03:20 +0200

    ajout de audrey

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ProductsService } from 'src/app/services/products.service';
+
 
 @Component({
   selector: 'app-login',
@@ -15,18 +15,11 @@ export class LoginComponent implements OnInit {
   isAuthenticated = false;
 
   //Service à créer
-  constructor(private router: Router, private productService: ProductsService) { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
-  login(logForm: any){
-    if(logForm.value.username == this.username && logForm.value.password == this.password){
-      this.router.navigate(["courses"]);
-      this.productService.isAuthenticated = true;
-    }else{
-      this.message = true;
-    }
-  }
+
 
 }
